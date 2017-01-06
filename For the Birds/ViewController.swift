a5288db1722fb65cd7e13bfc3c3c4afcef584f91
Stephen Furlani <stephen.furlani@gmail.com>
2017-01-06 13:44:08 -0500

    Initial Commit

---
+++ For the Birds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  For the Birds
+//
+//  Created by SFurlani on 1/6/17.
+//  Copyright © 2017 bizy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
